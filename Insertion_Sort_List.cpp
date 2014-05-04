83198566c50b09d0390fc95c82c4592c1e0b4400
19thhell <the19thhell@the19thhell-Inspiron-1120.(none)>
2014-05-04 14:44:47 +0800

    Insertion_Sort_List

---
+++ Insertion_Sort_List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode *insertionSortList(ListNode *head) {
+        ListNode **cur = &head, **iter = NULL;
+        while (*cur) {
+            iter = &head;
+            while ((*iter)->val < (*cur)->val && iter != cur)
+              iter = &((*iter)->next);
+            if (iter != cur) {
+                ListNode *tmp = *iter;
+                *iter = *cur;
+                *cur = (*cur)->next;
+                (*iter)->next = tmp;
+            }
+        }
+        return head;
+    }
+};
