7f52a8eaf69db666b9a98286694df40206ac3b2e
19thhell <the19thhell@the19thhell-Inspiron-1120.(none)>
2014-05-21 01:16:04 +0800

    119/151

---
+++ Gas_Station.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int start = 0, fuel = 0, n = gas.size();
+        for (int i = 0;i < n * 2;i++) {
+            fuel += gas[i % n];
+            if (fuel < cost[i % n]) {
+                start = (i + 1) % n;
+                fuel = 0;
+                continue;
+            }
+            else fuel -= cost[i % n];
+            if (start == (i + 1) % n)
+                return start;
+        }
+        return -1;
+    }
+};
