9dca8cbc78f424751bccf0994059cd1e4660e4ac
Xihua Yang <the19thhell@xihuas-mbp.home>
2014-09-27 01:10:33 -0400

    144/152

---
+++ Maximum_Product_Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	int maxProduct(int A[], int n) {
+		if (n == 0)
+			return 0;
+		int max_cur = A[0], min_cur = A[0], result = A[0];
+		for (int i = 1;i < n;i++) {
+		    int max_tmp = max_cur;
+		    max_cur = max(max(max_cur * A[i], A[i]), min_cur * A[i]);
+		    min_cur = min(min(max_tmp * A[i], A[i]), min_cur * A[i]);
+		    result = max(result, max_cur);
+		}
+		return result;
+	}
+};
