9330e84c0fe7470aa5f4fa94b632bdcc6eb89f58
Xihua Yang <the19thhell@xihuas-mbp.home>
2014-11-21 21:41:59 -0500

    152/157

--- Maximal_Rectangle.cpp
+++ Maximal_Rectangle.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    int maximalRectangle(vector<vector<char> > &matrix) {
+        if (matrix.size() == 0 || matrix[0].size() == 0)
+            return 0;
+        int row = matrix.size(), col = matrix[0].size(), result;
+        vector<vector<int> > height(row, vector<int>(col, 0));
+        for (int i = 0;i < col;i++)
+            if (matrix[0][i] == '1')
+                height[0][i] = 1;
+        result = largestRectangleArea(height[0]);
+        for (int i = 1;i < row;i++) {
+            for (int j = 0;j < col;j++) {
+                if (matrix[i][j] == '1')
+                    height[i][j] = height[i - 1][j] + 1;
+            }
+            result = max(result, largestRectangleArea(height[i]));
+        }
+        return result;
+    }
+private:
     int largestRectangleArea(vector<int> &height) {
         if (height.empty())
             return 0;
