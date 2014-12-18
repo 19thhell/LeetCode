2c944b288d89d4b3cb73036fc6a8ca1b911c258c
19thhell <yangxh92@gmail.com>
2014-12-18 15:53:09 -0500

    159/166, modified

--- Regular_Expression_Matching.cpp
+++ Regular_Expression_Matching.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     bool isMatch(const char *s, const char *p) {
-        if (!(*s)) {
-            if (!(*p)) {
-                return true;
-            }
-            else if (*(p + 1) == '*') {
-                return isMatch(s, p + 2);
-            }
-            else return false;
+        vector<vector<bool> > match(strlen(s) + 1, vector<bool>(strlen(p) + 1, false));
+        match[strlen(s)][strlen(p)] = true;
+        for (int i = 0;i < strlen(s);i++) {
+            match[i][strlen(p)] = false;
         }
-        else if (!(*p)) {
-            return false;
-        }
-        else if (*(p + 1) == '*') {
-            if (*s != *p && *p != '.') {
-                return isMatch(s, p + 2);
+        for (int i = strlen(p) - 2;i >= 0;i--) {
+            if (p[i + 1] == '*') {
+                match[strlen(s)][i] = match[strlen(s)][i + 2];
             }
-            for (NULL;*p && *(p + 1) == '*';p += 2) {
-                if (isMatch(s + 1, p)) {
-                    return true;
+        }
+        for (int j = strlen(p) - 1;j >= 0;j--) {
+            if (p[j] == '*') {
+                continue;
+            }
+            for (int i = strlen(s) - 1;i >= 0;i--) {
+                if (p[j + 1] != '*') {
+                    match[i][j] = (s[i] == p[j] || p[j] == '.') ? match[i + 1][j + 1] : false;
+                }
+                else if (match[i][j + 2])
+                    match[i][j] = true;
+                else {
+                    match[i][j] = (s[i] == p[j] || p[j] == '.') ? match[i + 1][j] : false;
                 }
             }
-            return isMatch(s, p);
         }
-        else {
-            if (*s == *p || *p == '.') {
-                return isMatch(s + 1, p + 1);
-            }
-            else return false;
-        }
+        return match[0][0];
     }
 };
