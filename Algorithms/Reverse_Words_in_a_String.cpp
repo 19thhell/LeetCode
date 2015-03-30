d68adba403856b724c40e1b6202c644b92fc6ddb
19thhell <yangxh92@gmail.com>
2015-03-30 17:44:09 -0400

    172/181, 6/11, 3/4

--- Algorithms/Reverse_Words_in_a_String.cpp
+++ Algorithms/Reverse_Words_in_a_String.cpp
@@ -1,46 +1,15 @@
-//Elegant way
 class Solution {
 public:
     void reverseWords(string &s) {
-        if (s.size() == 0)
-            return;
-        stringstream ss;
+        stringstream ss(s);
         string word;
-        stack<string> stk;
-        ss << s;
-        while (ss >> word) {
-            stk.push(word);
-        }
-        s = "";
-        while (!stk.empty()) {
-            s += stk.top();
-            stk.pop();
-            if (!stk.empty())
-                s += " ";
-        }
-    }
-};
-//Clunky
-class Solution {
-public:
-    void reverseWords(string &s) {
-        if (s.size() == 0)
+        ss >> s;
+        if (ss.fail()) {
+            s.clear();
             return;
-        int cnt = s.size() - 1;
-        string word, result;
-        while (true) {
-            while (cnt >= 0 && s[cnt] == ' ')
-                cnt--;
-            while (cnt >= 0 && s[cnt] != ' ')
-                word += s[cnt--];
-			if (word == "")
-				break;
-            if (result.size() > 0)
-                result += ' ';
-            for (int i = word.size() - 1;i >= 0;i--)
-				result += word[i];
-            word = "";
         }
-        s = result;
+        while (ss >> word) {
+            s = word + " " + s;
+        }
     }
 };
